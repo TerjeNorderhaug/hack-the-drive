617d734cab55524c499b10b37d32ba7b540fe243
Terje Norderhaug <terje@in-progress.com>
2015-01-10 15:29:06 -0800

    underscore in class path

--- src/hack-the-drive/web.clj
+++ src/hack-the-drive/web.clj
@@ -1,4 +1,4 @@
-(ns hack-the-drive.web
+(ns hack_the_drive.web
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.handler :refer [site]]
             [compojure.route :as route]
