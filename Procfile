9359e476d0338e21a6860e1ff41abec9747b3e3f
Terje Norderhaug <terje@in-progress.com>
2015-01-10 15:33:36 -0800

    underscores

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JVM_OPTS -cp target/hack-the-drive.jar clojure.main -m hack-the-drive.web
+web: java $JVM_OPTS -cp target/hack-the-drive.jar clojure.main -m hack_the_drive.web
---
+++ test/hack-the-drive/core_test.clj
@@ -0,0 +1,7 @@
+(ns hack_the_drive.core-test
+  (:require [clojure.test :refer :all]
+            [mojio-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
