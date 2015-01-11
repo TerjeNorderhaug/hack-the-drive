c45a2bdd27ee0b8a75ada8c9070d1a8469d49812
Terje Norderhaug <terje@in-progress.com>
2015-01-11 12:37:40 -0800

    test

--- src/hack_the_drive/web.clj
+++ src/hack_the_drive/web.clj
@@ -58,17 +58,19 @@
               bytes (or (:bytes content)
                         (if (get content :tempfile)
                           (IOUtils/toByteArray (io/input-stream (get content :tempfile)))))
-              id (store-media
+              data
                   (merge
                    {:bytes bytes}
                    (vehicle-details (get params "vehicle")) 
                    (if bytes {:intensity (thermal-intensity (image-from-bytes bytes) 20)})
                    (assoc content
-                          :vehicle (get params "vehicle"))))]
+                          :vehicle (get params "vehicle")))
+           ;   id (store-media data)
+          ]
           ; (render (success))
           ; (resp/redirect  (clojure.string/replace "/media/:id" #":id" (str id)))))
            ; (resp/redirect "/grid")
-          {:status 200 :headers {"Content-Type" "text/plain"} :body (pr-str params)}))
+          {:status 200 :headers {"Content-Type" "text/plain"} :body (pr-str data)}))
         {:store (byte-array-store)})
   (GET "/media/:id" [id]
     (render-media-response 
