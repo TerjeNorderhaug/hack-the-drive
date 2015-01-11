0674ca8b4d609f580d38452bebae31d285b6c4fb
Terje Norderhaug <terje@in-progress.com>
2015-01-10 17:14:04 -0800

    upload

--- src/hack_the_drive/web.clj
+++ src/hack_the_drive/web.clj
@@ -4,16 +4,31 @@
             [compojure.route :as route]
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
-            [environ.core :refer [env]]))
+            [ring.middleware [multipart-params :as mp]]
+            [environ.core :refer [env]]
+            [net.cgrand.enlive-html :refer [deftemplate]]))
 
 (defn splash []
   {:status 200
    :headers {"Content-Type" "text/plain"}
    :body (pr-str ["Hello" :from 'Heroku])})
 
+(defn render [t]
+  (apply str t))
+
+(deftemplate index "capture.html" [])
+
+(deftemplate upload-success "success.html" [])
+
+(defn upload-file [file]
+  ; (ds/copy (file :tempfile) (ds/file-str "file.out"))
+  (render (upload-success)))
+
 (defroutes app
-  (GET "/" []
-       (splash))
+  (GET  "/" [] (render (index)))
+  (mp/wrap-multipart-params
+    (POST "/capture" {params :params} 
+        (upload-file (get params "file"))))
   (ANY "*" []
        (route/not-found (slurp (io/resource "404.html")))))
 
